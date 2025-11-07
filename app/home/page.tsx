2ecfe1162fb07ebe6dbd5e6ea509cf560b15d0d4
oussama <oussamatght6@gmail.com>
2025-11-07 22:11:06 +0100

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -86,7 +86,7 @@ export default function HomePage() {
             <div className="min-h-screen flex items-center justify-center bg-transparent">
                 <div className="text-center">
                     <div className="animate-spin rounded-full h-16 w-16 border-b-4 border-blue-600 mx-auto"></div>
-                    <p className="mt-6 text-lg text-gray-600 font-medium">Loading events...</p>
+                    <p className="mt-6 text-lg text-gray-600 font-medium">Loading events and Clubs...</p>
                 </div>
             </div>
         );
